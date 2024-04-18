a81ac27941afdb709338d75e22826e0e315fd38f
Noah Fox <89417603+Noah-Fox@users.noreply.github.com>
2024-04-18 13:35:07 -0400

    Implement node class

--- tree_node.h
+++ tree_node.h
@@ -3,3 +3,36 @@
  * 
  * Contains class for a node in a tree
 */
+
+#include <string>
+#include <map>
+#include <vector>
+using namespace std;
+
+class TreeNode {
+    public:
+        TreeNode(string n, int w, map<string, TreeNode*>& nodeMap){
+            name = n;
+            weight = w;
+            parent = nullptr;
+            nodeMap[n] = this;
+        }
+
+        TreeNode(string n, int w, string p, map<string, TreeNode*>& nodeMap){
+            name = n;
+            weight = w;
+            if (nodeMap.find(p) != nodeMap.end()){
+                parent = nodeMap[p];
+                (nodeMap[p] -> children).push_back(this);
+            }else {
+                parent = nullptr;
+            }
+            nodeMap[n] = this;
+        }
+
+        string name;
+        int weight;
+        TreeNode* parent;
+        vector<TreeNode*> children;
+
+};
