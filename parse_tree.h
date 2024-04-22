c84d47c8cb86d418bba53d4767eadc96dca8c9a6
Noah Fox <89417603+Noah-Fox@users.noreply.github.com>
2024-04-22 17:02:05 -0400

    Implement parse tree classes

--- parse_tree.h
+++ parse_tree.h
@@ -8,6 +8,7 @@
 #include <map>
 #include <string>
 #include <set>
+#include <queue>
 #include <algorithm>
 #include <iostream>
 #include "tree_node.h"
@@ -34,7 +35,10 @@ class NumberConstant: public NumberExpression {
 
 class NumberVariable: public NumberExpression {
     public: 
-        NumberVariable(char* id){ varId = id; }
+        NumberVariable(char* id){ 
+            varId = id; 
+        }
+        void assignVar(intVar& iv){ iv.first = varId; }
         virtual int evaluate_expression(intVar& iv, stringVar& sv, nodeMap& tree){
             if (iv.first == varId){
                 return iv.second;
@@ -78,6 +82,7 @@ class StringConstant: public StringExpression {
 class StringVariable: public StringExpression {
     public: 
         StringVariable(char* id){ varId = id; }
+        void assignVar(stringVar& sv){ sv.first = varId; }
         virtual string evaluate_expression(intVar& iv, stringVar& sv, nodeMap& tree){
             if (sv.first == varId){
                 return sv.second;
@@ -116,6 +121,32 @@ class StringOfNumber: public StringExpression {
         NumberExpression* numExpr;
 };
 
+class StringList: public StringExpression {
+    public:
+        StringList(StringExpression* f, StringList* r){ first = f; rest = r; }
+        StringList(StringExpression* f){ first = f; rest = nullptr; }
+        virtual string evaluate_expression(intVar& iv, stringVar& sv, nodeMap& tree){
+            if (first == nullptr){
+                return "";
+            }
+            string toReturn = first -> evaluate_expression(iv,sv,tree);
+            if (toReturn == ""){
+                return "";
+            }
+
+            if (rest == nullptr){
+                first = new StringConstant("");
+            }
+            else {
+                first = new StringConstant(rest -> evaluate_expression(iv,sv,tree));
+            }
+            return toReturn;
+        }
+
+    private:
+        StringExpression* first;
+        StringList* rest;
+};
 
 class Statement {
  public:
@@ -130,10 +161,10 @@ class CompoundStatement: public Statement {
   }
   
   virtual void evaluate_statement(intVar& iv, stringVar& sv, nodeMap& tree) {
-    if (f!=NULL) {
+    if (f!=nullptr) {
       f->evaluate_statement(iv, sv, tree);
     }
-    if (r!=NULL) {
+    if (r!=nullptr) {
       r->evaluate_statement(iv, sv, tree);
     }
   }
@@ -173,9 +204,43 @@ class NumberForStatement: public Statement {
             loopStatement = l;
         }
 
+        virtual void evaluate_statement(intVar& iv, stringVar& sv, nodeMap& tree){
+            numVar -> assignVar(iv);
+            int start = startNum -> evaluate_expression(iv,sv,tree);
+            int end = endNum -> evaluate_expression(iv,sv,tree);
+            for (int i = start; i < end; i ++){
+                iv.second = i;
+                loopStatement -> evaluate_statement(iv,sv,tree);
+            }
+        }
+
     private:
         NumberVariable* numVar;
         NumberExpression* startNum;
         NumberExpression* endNum;
         CompoundStatement* loopStatement;
+};
+
+class StringForStatement: public Statement {
+    public:
+        StringForStatement(StringVariable* s, StringList* l, CompoundStatement* ls){
+            stringVar = s;
+            list = l;
+            loopStatement = ls;
+        }
+
+        virtual void evaluate_statement(intVar& iv, stringVar& sv, nodeMap& tree){
+            stringVar -> assignVar(sv);
+            sv.second = list -> evaluate_expression(iv,sv,tree);
+            while (sv.second != ""){
+                loopStatement -> evaluate_statement(iv,sv,tree);
+                sv.second = list -> evaluate_expression(iv,sv,tree);
+            }
+        }
+
+    private:
+        StringVariable* stringVar;
+        StringList* list;
+        CompoundStatement* loopStatement;
+        
 };
