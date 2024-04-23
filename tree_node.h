74a4ef421cadd72e5ea638c5b344b9b922ede8a4
Noah Fox <89417603+Noah-Fox@users.noreply.github.com>
2024-04-23 12:33:58 -0400

    Rewrite nonpointer tree node

--- tree_node.h
+++ tree_node.h
@@ -10,37 +10,42 @@
 #include <iostream>
 using namespace std;
 
-class TreeNode {
-    public:
-        TreeNode(string n, int w, map<string, TreeNode*>& nodeMap){
-            name = n;
-            weight = w;
-            parent = nullptr;
-            nodeMap[n] = this;
-        }
+struct TreeNode {
+    TreeNode(){
+        name = "";
+        weight = -1;
+        parent = "";
+        children = vector<string>();
+    }
+    
+    TreeNode(string n, int w, map<string, TreeNode>& nodeMap){
+        name = n;
+        weight = w;
+        parent = "";
+        children = vector<string>();
+        nodeMap[n] = *this;
+    }
 
-        TreeNode(string n, int w, string p, map<string, TreeNode*>& nodeMap){
-            name = n;
-            weight = w;
-            if (nodeMap.find(p) != nodeMap.end()){
-                parent = nodeMap[p];
-                (nodeMap[p] -> children).push_back(this);
-            }else {
-                parent = nullptr;
-            }
-            nodeMap[n] = this;
-        }
+    TreeNode(string n, int w, string p, map<string, TreeNode>& nodeMap){
+        name = n;
+        weight = w;
+        parent = p;
+        nodeMap[n] = *this;
+        children = vector<string>();
+        nodeMap[p].children.push_back(n);
+    }
 
-        string name;
-        int weight;
-        TreeNode* parent;
-        vector<TreeNode*> children;
-
+    string name;
+    int weight;
+    string parent;
+    vector<string> children;
 };
 
-void printTree(TreeNode* root){
-    cout << (root -> name) << "\n";
-    for (size_t i = 0; i < (root -> children).size(); i ++){
-        printTree(root -> children[i]);
+void printTree(string rootName, map<string, TreeNode>& nodeMap){
+    cout << rootName << "\n";
+    for (size_t i = 0; i < nodeMap[rootName].children.size(); i ++){
+        printTree(nodeMap[rootName].children[i], nodeMap);
     }
 }
+
+
